2c8558d6aee3bd1b8deffb518ea72ad4f9de3a9f
MaomiHz <maomihz@gmail.com>
2016-11-26 06:48:10 -0600

    Sudoku Solver: complete print_all_puzzles

--- Sudoku_solver/print_all_puzzles.m
+++ Sudoku_solver/print_all_puzzles.m
@@ -1,17 +1,39 @@
 %PRINT_ALL_PUZZLES solve all sudoku in a file
+%   PRINT_ALL_PUZZLES solves all sudoku puzzles in one file.
 
-puzzles = load_sudoku('hard.txt');
+% Uncomment to select one puzzle file
+% FILE = 'easy.txt';
+% FILE = 'simple.txt';
+% FILE = 'intermediate.txt';
+% FILE = 'hard.txt';
+FILE = 'custom.txt';
 
-c = 1;
+
+puzzles = load_sudoku(FILE);
+
+count = 1;
+
+% Iterate through all puzzles
 for i = puzzles
+    % Start timer
+    tic;
+
+    % run solver
     solution = solve_sudoku(i{1});
-    fprintf('%d: \n', c);
+
+    % Print solution puzzle
+    fprintf('Puzzle #%d: \n', count);
     print_sudoku(solution);
 
+    % Verify the solution
     if verify_sudoku(solution)
-        fprintf('SOLVED!!!\n');
+        fprintf('Solution Verified. ');
     else
-        fprintf('Not really...\n');
+        fprintf('Solution Incorrect. ');
     end
-    c = c + 1;
+
+    % Stop timer
+    tElapsed = toc;
+    fprintf('Time elapsed: %.6f seconds\n', tElapsed)
+    count = count + 1;
 end
