9ab31bf66644e2203a13f4a8fe6cd74cf7e8bd9d
MaomiHz <maomihz@gmail.com>
2016-11-26 06:48:43 -0600

    Sudoku solver: add more comments

--- Sudoku_solver/load_sudoku.m
+++ Sudoku_solver/load_sudoku.m
@@ -1,11 +1,21 @@
 function puzzles = load_sudoku(filename)
 %LOAD_SUDOKU load sudoku puzzles from file
+%   LOAD_SUDOKU(filename) loads sudoku from a file. In the file, each puzzle is
+%   represented by one line with 81 numbers. Empty space can be represented
+%   by either '0' or '.'. Lines that is not a valid puzzle line would be
+%   ignored. A cell of puzzles in one file would be returned.
+%
+%   LOAD_SUDOKU has the following inputs:
+%       filename - the name of the file to loads
+%
+%   LOAD_SUDOKU has the following outputs:
+%       puzzles - (cell) a list of puzzles represented by 2D array
 
     % open the puzzle file
     puzzleFile = fopen(filename, 'r');
 
     % Initialize cell
-    puzzles = {};
+    puzzles = cell(0);
     puzzleCount = 1;
 
     % Read line by line
@@ -18,16 +28,21 @@ function puzzles = load_sudoku(filename)
 
         % Ignore line length ~= 81
         if size(line, 2) == 81
+            % Create an empty puzzle board
             puzzle = zeros(9,9);
             for i = 1:81
                 c = line(i); % Read each character
+
                 if c == '.'
                     n = 0;
                 else
                     n = str2double(c);
                 end
+
                 puzzle(ceil(i / 9), mod(i - 1, 9) + 1) = n;
             end
+
+            % Add the loaded puzzle into list of puzzles
             puzzles{puzzleCount} = puzzle;
             puzzleCount = puzzleCount + 1;
         end
--- Sudoku_solver/print_sudoku.m
+++ Sudoku_solver/print_sudoku.m
@@ -1,5 +1,22 @@
 function [] = print_sudoku(sudokuPuzzle)
-%SUDOKU_PRINT(puzzle) print a sudoku puzzle to terminal
+%PRINT_SUDOKU print a sudoku puzzle to terminal
+%   PRINT_SUDOKU(sudokuPuzzle) prints a sudoku puzzle to terminal.
+%   Empty spaces are represented by .
+%
+%   Example:
+%    +-------+-------+-------+
+%    | 5 3 4 | 6 7 8 | 9 1 2 |
+%    | 6 7 2 | 1 9 5 | 3 4 8 |
+%    | 1 9 8 | 3 4 2 | 5 6 7 |
+%    +-------+-------+-------+
+%    | 8 5 9 | 7 6 1 | 4 2 3 |
+%    | 4 2 6 | 8 5 3 | 7 9 1 |
+%    | 7 1 3 | 9 2 4 | 8 5 6 |
+%    +-------+-------+-------+
+%    | 9 6 1 | 5 3 7 | 2 8 4 |
+%    | 2 8 7 | 4 1 9 | 6 3 5 |
+%    | 3 4 5 | 2 8 6 | 1 7 9 |
+%    +-------+-------+-------+
 
     if nargin ~= 1
         error('Argument number should be at least 1! ');
