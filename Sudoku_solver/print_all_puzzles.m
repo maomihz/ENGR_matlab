bdfb9e6dc1b12ecac8189b14902b644d91f346d5
MaomiHz <maomihz@gmail.com>
2016-11-28 12:47:39 -0600

    Sudoku Solver: Modify print_all_puzzles to add options to configure

--- Sudoku_solver/print_all_puzzles.m
+++ Sudoku_solver/print_all_puzzles.m
@@ -5,8 +5,13 @@
 % FILE = 'puzzles/easy.txt';
 % FILE = 'puzzles/simple.txt';
 % FILE = 'puzzles/intermediate.txt';
-% FILE = 'puzzles/hard.txt';
-FILE = 'puzzles/custom.txt';
+FILE = 'puzzles/hard.txt';
+% FILE = 'puzzles/custom.txt';
+% FILE = 'puzzles/proj_euler.txt';
+
+PRINT_ORIGINAL = true; % Printing unsolved puzzle
+VERIFY_SOLUTION = true;
+TIMING = true; % Printing time elapsed
 
 
 puzzles = load_sudoku(FILE);
@@ -23,17 +28,26 @@ for i = puzzles
 
     % Print solution puzzle
     fprintf('Puzzle #%d: \n', count);
+    if PRINT_ORIGINAL
+        print_sudoku(i{1});
+    end
     print_sudoku(solution);
 
     % Verify the solution
-    if verify_sudoku(solution)
-        fprintf('Solution Verified. ');
+    if VERIFY_SOLUTION
+        if verify_sudoku(solution)
+            fprintf('Solution Verified. ');
+        else
+            fprintf('Solution Incorrect. ');
+        end
     else
-        fprintf('Solution Incorrect. ');
+        fprintf('\n');
     end
 
     % Stop timer
     tElapsed = toc;
-    fprintf('Time elapsed: %.6f seconds\n', tElapsed)
+    if TIMING
+        fprintf('Time elapsed: %.6f seconds\n', tElapsed);
+    end
     count = count + 1;
 end
